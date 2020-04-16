eaa3774f9a71b56c107430ad44d58f36fa9a20cf
kamillabs <kamillabaran00@gmail.com>
2020-04-16 07:57:31 +0000

    first assignment

---
+++ day-1.py
@@ -0,0 +1,13 @@
+#functions to perform the following maths operations
+def addTwovalues(value_one,value_two):
+    return value_one + value_two
+print(addTwovalues(2,3))
+def multiplyvalues(value_three,value_four):
+    return value_three * value_four
+print(multiplyvalues(3,6))
+def dividevalues(value_five,value_six):
+   return value_five / value_six
+print(dividevalues(9,3))
+def substractvalues(value_seven,value_eight):
+    return value_seven - value_eight
+print(substractvalues(3,1))

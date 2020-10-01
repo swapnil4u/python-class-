0e72fb3fe56dd9d34688cefec3974aef83c165d5
swapnil4u <65157253+swapnil4u@users.noreply.github.com>
2020-10-02 00:34:44 +0530

    Refined code and take input from user

--- day-1.py
+++ day-1.py
@@ -1,13 +1,22 @@
 #functions to perform the following maths operations
 def addTwovalues(value_one,value_two):
     return value_one + value_two
-print(addTwovalues(2,3))
+
 def multiplyvalues(value_three,value_four):
     return value_three * value_four
-print(multiplyvalues(3,6))
+
 def dividevalues(value_five,value_six):
    return value_five / value_six
-print(dividevalues(9,3))
+
 def substractvalues(value_seven,value_eight):
     return value_seven - value_eight
-print(substractvalues(3,1))
+
+# Input two numbers
+a = int(input("\nFirst number :"))
+b = int(input("\nSecond number :"))
+
+# Display outputs after operations
+print(addTwovalues(a,b))
+print(multiplyvalues(a,b))
+print(dividevalues(a,b))
+print(substractvalues(a,b))
